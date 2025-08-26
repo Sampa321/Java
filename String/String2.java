bebfc7df26d62a99f657e0246323723307a81351
Sampa Nayak <sampanayak987@gmail.com>
2025-08-27 00:56:46 +0530

    Add files via upload

---
+++ String/String2.java
@@ -0,0 +1,11 @@
+package ClassPractice.Arrays_String.String;
+
+public class String2 {
+    public static void main(String[] args) {
+        String temp="Now I am learning strings in java";
+       System.out.println(temp.indexOf("java".compareTo("jAva")));
+        String a=" hello world";
+        int b=a.indexOf("w");
+        System.out.println(b);
+    }
+}
