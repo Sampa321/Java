1f704688706ff9b4c70d4d469a4abfa8594f6802
Sampa Nayak <sampanayak987@gmail.com>
2025-09-07 00:45:04 +0530

    Update A.java

--- Method/A.java
+++ Method/A.java
@@ -1,1 +1,17 @@
+package ClassPractice.Method;
+//Instance method
+public class A {
+    int a;
+    int b;
+    public void add(){
+        System.out.println("sum:"+(a+b));
+    }
+
+    public static void main(String[] args) {
+        A obj1=new A();
+        obj1.a=100;
+        obj1.b=50;
+        obj1.add();
+    }
+}                
 
