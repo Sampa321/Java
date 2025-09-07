dfd93bbdc3be9d9d2c754adce790e06eeec43e76
Sampa Nayak <sampanayak987@gmail.com>
2025-09-08 01:43:45 +0530

    Add files via upload

---
+++ Multithreading/Example1/B.java
@@ -0,0 +1,10 @@
+package ClassPractice.Multithreading.Example1;
+
+public class B extends Thread{
+    public void run(){
+        for(int i=0;i<100;i++)
+        {
+            System.out.println("Hello");
+        }
+    }
+}
