fbba3296da3226923706fa03c8448abe7c8aa730
Sampa Nayak <sampanayak987@gmail.com>
2025-09-09 21:35:32 +0530

    Add files via upload

---
+++ Oops/ConstructorCode/OverloadingConstructor.java
@@ -0,0 +1,16 @@
+package ClassPractice.OOPS.ConstructorsCode.ConstructorMainCode;
+
+public class OverloadingConstructor {
+    String name;
+    int age;
+    // No argument Constructor
+    OverloadingConstructor(){
+        this.name=name;
+        this.age=age;
+    }
+
+
+    public static void main(String[] args) {
+        OverloadingConstructor obj1=new OverloadingConstructor();
+    }
+}
