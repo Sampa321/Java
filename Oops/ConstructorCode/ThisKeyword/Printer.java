369bdfda8e172dcc2dd58d006f6f76551bcfec9e
Sampa Nayak <sampanayak987@gmail.com>
2025-09-10 17:04:37 +0530

    Add files via upload

---
+++ Oops/ConstructorCode/ThisKeyword/Printer.java
@@ -0,0 +1,8 @@
+package ClassPractice.OOPS.ConstructorsCode.ThisKeyword;
+
+public class Printer {
+    public void printPersonInfo(CurrentInstanceArgument o1)
+    {
+        System.out.println("Person name:"+o1.name);
+    }
+}
