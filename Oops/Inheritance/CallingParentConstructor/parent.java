5dd31847a0c43761aabe98ae586c9a80a365e001
Sampa Nayak <sampanayak987@gmail.com>
2025-09-11 13:29:48 +0530

    Add files via upload

---
+++ Oops/Inheritance/CallingParentConstructor/parent.java
@@ -0,0 +1,11 @@
+package ClassPractice.OOPS.Inheritance.CallingParentConstructor;
+
+class parent {
+    parent() {
+        System.out.println("THis is parent class constructor");
+    }
+
+    void show() {
+        System.out.println("This is parent class");
+    }
+}
