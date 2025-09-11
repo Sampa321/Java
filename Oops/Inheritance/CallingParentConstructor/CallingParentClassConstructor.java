b680e5bc2a7ddf19765533e80900cea11afe69b4
Sampa Nayak <sampanayak987@gmail.com>
2025-09-11 13:27:36 +0530

    Create CallingParentClassConstructor.java

---
+++ Oops/Inheritance/CallingParentConstructor/CallingParentClassConstructor.java
@@ -0,0 +1,8 @@
+package ClassPractice.OOPS.Inheritance.CallingParentConstructor;
+
+public class CallingParentClassConstructor {
+    public static void main(String[] args) {
+        child ch=new child();
+        ch.show();
+    }
+}
