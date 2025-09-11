0116a9b7270adebefca91d007bcc1a35697e7a11
Sampa Nayak <sampanayak987@gmail.com>
2025-09-11 13:33:11 +0530

    Create A.java

---
+++ Oops/Inheritance/ConstructorCallOrder/A.java
@@ -0,0 +1,11 @@
+package ClassPractice.OOPS.Inheritance.ConstructorCallOrder;
+
+public class A {
+    int c;
+    int d;
+    A()
+    {
+        this.c=5;
+        System.out.println("This is A class constructor");
+    }
+}
