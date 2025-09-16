053148ea4c457e76a8069ac9e2a1ba1b32d4b50d
Sampa Nayak <sampanayak987@gmail.com>
2025-09-17 00:29:25 +0530

    Add files via upload

---
+++ W3SchoolSomeQuestion/JavaMethod/JavaRecursive/Factorial.java
@@ -0,0 +1,22 @@
+package HomePractice.W3School.JavaMethod.JavaRecursion;
+
+/*
+Factorial of a number using recursion.
+ */
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n=sc.nextInt();
+        System.out.println("Factorial of 5 is " + factorial(n));
+    }
+    static int factorial(int n) {
+        if (n > 1) {
+            return n * factorial(n - 1);
+        } else {
+            return 1;
+        }
+    }
+}
